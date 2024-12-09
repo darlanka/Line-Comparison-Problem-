79f7d86bb0f867aee8894850b1d7eb3d09e5382b
Praneeth <darlanka21_ug@ece.nits.ac.in>
2024-12-10 03:29:46 +0530

    Added: camparison between two lines

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to line comparision Computation Program on Master branch");
         Scanner sc = new Scanner(System.in);
-        System.out.println("the first point :");
+        System.out.println("the first point of first line:");
         int px1 = sc.nextInt();
         int py1 = sc.nextInt();
-        System.out.println("now the second point : ");
+        System.out.println("now the second point of first line : ");
         int px2 = sc.nextInt();
         int py2 = sc.nextInt();
+
+        System.out.println("the first point of second line :");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("now the second point of second line: ");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
         distance d1 = new distance();
         double ans = d1.dis(px1,py1,px2,py2);
         // checking if two lines are equal or not! by using the end points
         // so dis (x1,y1,x2,y2) == dis(x2,y2,x1,y1) must be equal
-        if(d1.dis(px1,py1,px2,py2) == d1.dis(px2,py2,px1,py1)){
+        double dis1 = d1.dis(px1,py1,px2,py2);
+        double dis2 = d1.dis(x1,y1,x2,y2);
+        if(dis1 == dis2){
             System.out.println("Both lines are equal!");
         }
-        else{
-            System.out.println("both are not equal!");
+        else if (dis1 > dis2){
+            System.out.println("Line 1 is greater than second line");
+        }
+        else {
+            System.out.println("Second line is greater than first line");
         }
     }
 }
